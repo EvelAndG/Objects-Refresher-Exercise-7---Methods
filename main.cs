5258b4c1d725cc9934e04f9aba5dd0868e48e419
Gage Eveland <42362153+EvelAndG@users.noreply.github.com>
2021-10-04 21:13:14 +0000

    Complete?

--- main.cs
+++ main.cs
@@ -4,9 +4,22 @@ class MainClass
 {
   public static void Main (string[] args) 
 	{
-	Crypto crypto1 = new Crypto("Etherium", "ETH", 100, 2);
 
-	Console.WriteLine(crypto1.Name);
+	
+	Crypto crypto1 = new Crypto("Etherium", "ETH", 100, 2.3453);
+
+	Crypto crypto2 = new Crypto("Dogecoin", "DOGE", 0.2426, 1);
+
+	Crypto crypto3 = new Crypto("Litecoin", "LTC", 168.30, 12);
+
+		double sumOfCyrpto = crypto1.Product() + crypto2.Product() + crypto3.Product();
+		
+
+	Console.WriteLine(crypto1);
+	Console.WriteLine(crypto2);
+	Console.WriteLine(crypto3);
+
+	Console.WriteLine("Total Value for all Crypto Owned = "+sumOfCyrpto);
 
 	Console.WriteLine(crypto1.Product());
 
@@ -45,10 +58,15 @@ class MainClass
 
 	}
 
-
+public override string ToString()
+	{
+		return "\nName: " + this.Name+"\nToken Name: " +this.TokenName+"\nCurrent Price: "+this.CurrentPrice+"\nAmount Owned: "+this.AmountOwned;
+	}
 
 
 	}
+
+	
 	
 
 	
